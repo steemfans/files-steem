f88f5200d67744e1df884a1ebbe023993da87879
ety001 <ety001@users.noreply.github.com>
2021-06-03 23:45:38 +0800

    Update FileBox.vue

--- src/components/FileBox.vue
+++ src/components/FileBox.vue
@@ -12,7 +12,7 @@
           width="300">
           <template slot-scope="scope">
             <i class="el-icon-document" v-if="scope.row.fileType === 'flie'"></i>
-            <span style="margin-left: 10px"><a :href="scope.row.fileType === 'flie' ? scope.row.filePath : '#'">{{ scope.row.fileName }}</a></span>
+            <span style="margin-left: 10px"><a v-bind:href="scope.row.fileType === 'flie' ? scope.row.filePath : '#'">{{ scope.row.fileName }}</a></span>
           </template>
         </el-table-column>
         <el-table-column
